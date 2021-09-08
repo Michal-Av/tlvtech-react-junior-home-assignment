543f350551699edcd6424093c116bf671fd34f38
Michal Avraham <59959727+Michal-Av@users.noreply.github.com>
2021-09-09 00:22:31 +0300

    Update game-service.js

--- src/game-service.js
+++ src/game-service.js
@@ -13,3 +13,22 @@ export async function deleteGame(gameId) {
   const resp = await fetch(`${apiUrl}/game/${gameId}`, { method: 'DELETE' });
   return await resp.json();
 }
+
+export async function addGames(gameObj) {
+  const resp = await fetch(`${apiUrl}/game`, { method: 'POST', headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json; charset=utf-8"
+  },
+  body: JSON.stringify(gameObj) });
+  return await resp.json();
+}
+
+export async function editGame(gameId, gameObj) {
+  const resp = await fetch(`${apiUrl}/game/${gameId}`, { method: 'PUT', headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json; charset=utf-8"
+  },
+   body: JSON.stringify(gameObj) });
+  return await resp.json();
+}
+
